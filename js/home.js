5e5f777ae9c423191763fe024a5196b231986f4a
thallya-nt <thallyanatielly.fs@gmail.com>
2026-04-05 20:20:42 -0300

    feat: contador de clientes funcional

--- js/home.js
+++ js/home.js
@@ -4,3 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
     dateFormat: "d-m-Y",
   });
 });
+
+function contadorClientes() {
+  //BUSCA A LISTA DE CLIENTES
+  let dados = localStorage.getItem("clientes");
+
+  let total = 0;
+  if (dados) {
+    const arrayClientes = JSON.parse(dados);
+    total = arrayClientes.length;
+  }
+
+  const contador = document.getElementById("contador");
+  if (contador) {
+    contador.textContent = total;
+    console.log("contador atualizado!");
+  } else {
+    console.log("erro ao atualizar contador. verifique o código.");
+  }
+}
+// CHAMA A FUNÇÃO ASSIM QUE A PÁGINA É CARREGADA
+window.addEventListener("DOMContentLoaded", contadorClientes);
