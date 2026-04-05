96256983c77d04de4d37ed3c9662055de2b5a796
thallya-nt <thallyanatielly.fs@gmail.com>
2026-04-05 19:47:22 -0300

    feat: cadastro de cliente em localstorage

--- js/cadClients.js
+++ js/cadClients.js
@@ -1,32 +1,52 @@
+console.log("Arquivo carregado com sucesso!");
+
 const formCliente = document.querySelector(".formularioCliente");
 
-formCliente.addEventListener("submit", function (cad) {
-  cad.preventDefault();
+function cadastrarCliente(event) {
+  //EVITA O RELOUD DA PÁGINA
+  event.preventDefault();
 
-  const nome = document.getElementById("nomeC").value;
-  const tipo = document.getElementById("opc").value;
+  //VARIÁVEIS DE CADASTRO DO CLIENTE
+  let nome = document.getElementById("nome").value;
+  let tipo = document.getElementById("opc").value;
+  let email = document.getElementById("email").value;
+  let referencia = document.getElementById("ref").value;
+  let endereco = document.getElementById("end").value;
+  let contato = document.getElementById("cntt").value;
 
-  if (!nome) {
-    return alert("O nome não pode estar vazio");
+  //TESTE PARA EVITAR QUE CAMPOS ESSENCIAIS FIQUEM VAZIOS
+  if (!nome || !email || !referencia || !contato) {
+    return alert("Um campo obrigatório está vazio.");
   }
 
-  const dadoCliente = {
-    name: nome,
-    tipo: tipo,
-    dataCadastro: new Date().toISOString(),
+  //CRIA O OBJETO CLIENTE
+  const novoCLiente = {
+    id: Date.now(),
+    nome,
+    tipo,
+    email,
+    referencia,
+    endereco,
+    contato,
   };
-  console.log("Dados prontos para envio:", dadoCliente);
 
-  cadastarCliente(dadoCliente);
-});
+  //CRIA UMA LISTA COM AS INFORMAÇÕES
+  const clientesList = JSON.parse(localStorage.getItem("clientes")) || [];
 
-function cadastarCliente(dados) {
-  const mensagem = document.getElementById("m");
+  clientesList.push(novoCLiente);
+  localStorage.setItem("clientes", JSON.stringify(clientesList));
+
+  //MOSTRA UMA MENSAGEM APÓS O CADASTRO BEM SUCEDIDO
+  const mensagem = document.getElementById("msg");
   const p = document.createElement("p");
   p.textContent = "Cliente cadastrado com sucesso! ✅";
   mensagem.appendChild(p);
 
+  //VOLTA A HOMEPAGE
   setTimeout(() => {
     window.location.href = "homePage.html";
   }, 2000);
+}
+if (formCliente) {
+  formCliente.addEventListener("submit", cadastrarCliente);
 }
