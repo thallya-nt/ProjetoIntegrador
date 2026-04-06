8e466e75ee241c4f281c15f2af4f9ae680bda29d
thallya-nt <thallyanatielly.fs@gmail.com>
2026-04-06 12:12:22 -0300

    feat: modal de listagem de clientes

--- js/home.js
+++ js/home.js
@@ -25,3 +25,39 @@ function contadorClientes() {
 }
 // CHAMA A FUNÇÃO ASSIM QUE A PÁGINA É CARREGADA
 window.addEventListener("DOMContentLoaded", contadorClientes);
+
+//POPUP DE CLIENTES
+const openBt = document.querySelectorAll('.openMd');
+
+openBt.forEach(button => {
+  button.addEventListener('click', () =>{
+    const modalId = button.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+
+    const clientesLista = document.getElementById('cList')
+    const clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+    clientesLista.innerHTML = "";
+
+    if(clientes.length === 0) {
+      clientesLista.innerHTML = "<p> Nenhum cliente cadastrado!</p>";
+    } else{
+      clientes.forEach((cliente) => {
+      const p = document.createElement("p");
+      p.innerHTML = `<strong>${cliente.nome}</strong> :  tipo - ${cliente.tipo} <br> email - ${cliente.email} <br> referência - ${cliente.referencia} <br> endereço - ${cliente.endereco} <br> contato - ${cliente.contato}`;
+
+      clientesLista.appendChild(p);
+    });
+  }
+   modal.showModal();
+  });
+});
+
+const closeBt = document.querySelectorAll('.closeMd');
+
+closeBt.forEach(button => {
+  button.addEventListener('click', () =>{
+     const modalId = button.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+   modal.close();
+  })
+})
